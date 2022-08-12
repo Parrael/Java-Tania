2796b8d1780b666aca7faa1a8f3ce35f08463874
Catarina-de-PD <catarina.f.bonifacio@gmail.com>
2022-08-12 09:07:09 -0300

    ExercicioMVC

---
+++ revisao mvc/exemploMVC2/src/control/AlunoController.java
@@ -0,0 +1,29 @@
+package control;
+import java.util.ArrayList;
+import model.Aluno;
+
+public class AlunoController {
+private ArrayList<Aluno> listaAl; 
+public AlunoController()
+{
+listaAl = new ArrayList<>();
+}
+public void cadastrar (int ra, String nome)
+{
+    Aluno a1 = new Aluno (ra, nome);
+    listaAl.add(a1);
+}      
+public ArrayList<Aluno> mostraTodos()
+{
+    return listaAl;
+}
+public Aluno mostrarEspecifico (int ra)
+{
+    for(Aluno a: listaAl)
+    {
+        if(a.getRa()== ra)
+            return a;
+    }
+    return null;
+}
+}
