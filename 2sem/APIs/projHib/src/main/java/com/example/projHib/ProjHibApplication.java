fd5ff66b1193eae1cec5d4b745aa4cafc8fe9686
Catarina-de-PD <catarina.f.bonifacio@gmail.com>
2022-10-21 09:09:25 -0300

    sla

---
+++ 2sem/APIs/projHib/src/main/java/com/example/projHib/ProjHibApplication.java
@@ -0,0 +1,13 @@
+package com.example.projHib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjHibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjHibApplication.class, args);
+	}
+
+}
